453af0580d82cc0e448d6b703607a7566250f265
CarlJung777 <125782812+CarlJung777@users.noreply.github.com>
2024-10-29 00:49:03 +0800

    lu

--- CollectionTypes.swift
+++ CollectionTypes.swift
@@ -1,7 +1,7 @@
 //#####   Collection Types   #####//
-// Array
-// Set
-// Dictionary
+// @@ Array      @@
+// @@ Set        @@
+// @@ Dictionary @@
 // the collection  will be mutable
 
 //#####   Creating an Empty Array   #####//
@@ -19,5 +19,24 @@ var sixDoubles = fourDoubles + anotherThreeDoubles
 print(sixDoubles) // [0.0, 0.0, 0.0, 0.0, 2.5, 2.5, 2.5]
 
 //#####   Creating an Array with an Array Literal  #####//
-var bucketList: [String] = ["fly a plane", "speak english fluently"]
-var sameBucketList = ["fly a plane", "speak english fluently"]
+var bucketList: [String] = ["fly a plane", "write a book"]
+var sameBucketList = ["fly a plane", "write a book"]
+
+//#####   Accessing and Modifying an Array  #####//
+print(sameBucketList.count)
+
+if bucketList.isEmpty {
+    print("The bucke tList is empty")
+} else {
+    print("The bucke List isn't empty")
+}
+
+bucketList.append("buy a plane")
+// Alternatively
+bucketList += ["speak japanese"]
+
+var firstWish = bucketList[0]
+bucketList[2] = "owning a building"
+
+// replacing in range
+bucketList[2...3] = ["Bananas", "Apples"]
